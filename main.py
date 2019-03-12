fd140dc530e22d501247d4b2df984b512fac6e00
aloneik <aloneik@yandex.ru>
2019-03-12 15:39:28 +0300

    Improve response parsing implementation

--- main.py
+++ main.py
@@ -5,22 +5,15 @@ from sys import argv
 
 
 URL = "http://www.flybulgarien.dk/en/search"
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:65.0) Gecko/"
-    "20100101 Firefox/65.0"
-}
 MAX_PASSENGERS = 8
-NOT_ENOGHT_PARAMETERS = """You entered <3 parameters.
-Please input departure IATA code, arrival IATA code
-and departure date."""
 
 
 """ def get_flights(
     departure_city_code, arrival_city_code,
-    departure_date, return_date=None, passengers=1
+    departure_date, arrival_date=None, passengers=1
         ):
     print departure_city_code, arrival_city_code
-    print departure_date, return_date
+    print departure_date, arrival_date
     payload = {
         "lang": 2,
         "departure-city": departure_city_code,
@@ -43,51 +36,89 @@ and departure date."""
             print "FFFFUUUUUUCCCCCCKKKK222222"
         print iframe_response.request.headers
         parsed_iframe = html.fromstring(iframe_response.text)
-        filghts_table, = parsed_iframe\
+        flights_table, = parsed_iframe\
             .xpath("//table[@id=\"flywiz_tblQuotes\"]")
-        print filghts_table.xpath("./tr/th[@colspan=6]/text()")
+        print flights_table.xpath("./tr/th[@colspan=6]/text()")
  """
 
 
 def scrape():
-    if validation(*argv[1:]):
-        # request(...)
-        session = Session()
-        request = build_request(session, URL, parse_user_input(*argv[1:]))
-        try:
-            request = build_request(
-                session,
-                fetch_iframe_url(session.send(request))
-                )
-            return parse_response(session.send(request))
-        # TODO concoct exception types to catch
-        except:
-            # check_errors(...)
-            pass
+    if not is_valid_input(*argv[1:]):
+        return
+    session = Session()
+    request = build_request(session, URL, user_input=argv[1:])
+    result = None
+    try:
+        second_request = build_request(
+            session,
+            parse_response(session.send(request))
+        )
+        result = parse_response(session.send(second_request))
+    # TODO concoct exception types to catch
+    except Exception as e:
+        print e
+        # check_errors(...)
+        check_errors()
+        pass
+    return result
 
 
-def build_request(session, url, payload=None):
+def check_errors():
+    pass
+
+
+def build_request(session, url, payload=None, user_input=None):
+    if user_input:
+        user_input = parse_user_input(*user_input)
+        if payload:
+            payload.update(user_input)
+        else:
+            payload = user_input
     request = Request("GET", url, params=payload)
     return session.prepare_request(request)
-
-
-def fetch_iframe_url(response):
-    parsed_body = html.fromstring(response.text)
-    iframe_url, = parsed_body.xpath("//iframe/@src")
-    if iframe_url:
-        return iframe_url
 
 
 # TODO complete implementation
 def parse_response(response):
     parsed_body = html.fromstring(response.text)
-    filghts_table, = parsed_body.xpath("//table[@id=\"flywiz_tblQuotes\"]")
-    print filghts_table.xpath("./tr/th[@colspan=6]/text()")
+    print "i"
+    iframe_elements = parsed_body.xpath("//iframe/@src")
+    if iframe_elements:
+        iframe_url, = iframe_elements
+        if iframe_url:
+            print iframe_url
+            return iframe_url
+    print "t", parsed_body.xpath("//table[@id=\"flywiz_tblQuotes\"]")
+    with open("t.html", "w") as f:
+        f.write(response.text)
+    flights_table, = parsed_body.xpath("//table[@id=\"flywiz_tblQuotes\"]")
+    print flights_table.xpath("./tr/th[@colspan=6]/text()")
+    flights = zip(
+        flights_table.xpath("./tr[contains(@id,'flywiz_rinf')]"),
+        flights_table.xpath("./tr[contains(@id,'flywiz_rprc')]")
+    )
+    flights_info = []
+    flights_info.append([
+        flight[0].xpath("./td/text()")[1:] + flight[1].xpath("./td/text()")
+        for flight in flights
+    ])
+    print flights_table.xpath("./tr[contains(@id,'flywiz_irinf')]")
+    return_flights = zip(
+        flights_table.xpath("./tr[contains(@id,'flywiz_irinf')]"),
+        flights_table.xpath("./tr[contains(@id,'flywiz_irprc')]")
+    )
+    print return_flights
+    if return_flights:
+        flights_info.append([
+            flight[0].xpath("./td/text()")[1:] + flight[1].xpath("./td/text()")
+            for flight in return_flights
+        ])
+    return flights_info
 
 
 def parse_user_input(
     departure_city_code, arrival_city_code,
-    departure_date, return_date=None, passengers=1
+    departure_date, arrival_date=None, passengers=1
         ):
     payload = {
         "lang": 2,
@@ -98,15 +129,19 @@ def parse_user_input(
         "adults-children": passengers,
         "search": "Search!"
     }
+    if arrival_date:
+        payload["arrival-date"] = arrival_date
     return payload
 
 
-def validation(
+def is_valid_input(
     departure_city_code, arrival_city_code,
-    departure_date, return_date=None, passengers=1
+    departure_date, arrival_date=None, passengers=1
         ):
     if len(argv) < 4:
-        print NOT_ENOGHT_PARAMETERS
+        print """You entered <3 parameters.
+                Please input departure IATA code, arrival IATA code
+                and departure date."""
         return False
     else:
         dep_date = date(
@@ -114,15 +149,14 @@ def validation(
             int(departure_date.split(".")[1]),
             int(departure_date.split(".")[0])
         )
-        # Write good check
         if date.today() > dep_date:
             print "Departure date must be >= today"
             return False
-        if return_date:
+        if arrival_date:
             ret_date = date(
-                int(return_date.split(".")[2]),
-                int(return_date.split(".")[1]),
-                int(return_date.split(".")[0])
+                int(arrival_date.split(".")[2]),
+                int(arrival_date.split(".")[1]),
+                int(arrival_date.split(".")[0])
             )
             print ret_date
             if ret_date < dep_date:
@@ -134,5 +168,4 @@ def validation(
     return True
 
 
-validation(*argv[1:])
-scrape()
+print scrape()
