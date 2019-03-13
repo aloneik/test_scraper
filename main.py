0522ad2b29e3017843510b9a4a4319d650b37a45
aloneik <aloneik@yandex.ru>
2019-03-13 11:32:09 +0300

    Change url to scrape

--- main.py
+++ main.py
@@ -4,15 +4,7 @@ from lxml import html
 from sys import argv
 
 
-URL = "http://www.flybulgarien.dk/en/search"
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:65.0) Gecko/"
-    "20100101 Firefox/65.0"
-}
-MAX_PASSENGERS = 8
-NOT_ENOGHT_PARAMETERS = """You entered <3 parameters.
-Please input departure IATA code, arrival IATA code
-and departure date."""
+URL = "https://apps.penguin.bg/fly/quote3.aspx"
 
 
 """ def get_flights(
@@ -49,21 +41,29 @@ and departure date."""
  """
 
 
-def scrape():
-    if validation(*argv[1:]):
-        # request(...)
-        session = Session()
-        request = build_request(session, URL, parse_user_input(*argv[1:]))
-        try:
-            request = build_request(
-                session,
-                fetch_iframe_url(session.send(request))
-                )
-            return parse_response(session.send(request))
-        # TODO concoct exception types to catch
-        except:
-            # check_errors(...)
-            pass
+def scrape(
+    departure_city_code, arrival_city_code,
+    departure_date, return_date=None, passengers=1
+        ):
+    user_input = {
+        "departure_city_code": departure_city_code,
+        "arrival_city_code": arrival_city_code,
+        "departure_date": departure_date,
+        "return_date": return_date,
+        "passengers": passengers
+    }
+    if not is_valid_input(**user_input):
+        return
+    session = Session()
+    request = build_request(session, URL, parse_user_input(**user_input))
+    result = None
+    try:
+        result = parse_response(session.send(request))
+    # TODO concoct exception types to catch
+    except Exception as e:
+        print e.message
+        # check_errors(...)
+    return result
 
 
 def build_request(session, url, payload=None):
@@ -71,11 +71,11 @@ def build_request(session, url, payload=None):
     return session.prepare_request(request)
 
 
-def fetch_iframe_url(response):
-    parsed_body = html.fromstring(response.text)
-    iframe_url, = parsed_body.xpath("//iframe/@src")
-    if iframe_url:
-        return iframe_url
+# def fetch_iframe_url(response):
+#     parsed_body = html.fromstring(response.text)
+#     iframe_url, = parsed_body.xpath("//iframe/@src")
+#     if iframe_url:
+#         return iframe_url
 
 
 # TODO complete implementation
@@ -90,34 +90,36 @@ def parse_user_input(
     departure_date, return_date=None, passengers=1
         ):
     payload = {
-        "lang": 2,
-        "departure-city": departure_city_code,
-        "arrival-city": arrival_city_code,
-        "reserve-type": "",
-        "departure-date": departure_date,
-        "adults-children": passengers,
-        "search": "Search!"
+        "ow": "",
+        "lang": "en",
+        "depdate": departure_date,
+        "aptcode1": departure_city_code,
+        "aptcode2": arrival_city_code,
+        "paxcount": passengers,
+        "infcount": ""
     }
     return payload
 
 
-def validation(
+def is_valid_input(
     departure_city_code, arrival_city_code,
     departure_date, return_date=None, passengers=1
         ):
+    result = True
     if len(argv) < 4:
-        print NOT_ENOGHT_PARAMETERS
-        return False
+        print """You entered <3 parameters.
+            Please input departure IATA code, arrival IATA code
+            and departure date."""
+        result = False
     else:
         dep_date = date(
             int(departure_date.split(".")[2]),
             int(departure_date.split(".")[1]),
             int(departure_date.split(".")[0])
         )
-        # Write good check
         if date.today() > dep_date:
             print "Departure date must be >= today"
-            return False
+            result = False
         if return_date:
             ret_date = date(
                 int(return_date.split(".")[2]),
@@ -127,12 +129,11 @@ def validation(
             print ret_date
             if ret_date < dep_date:
                 print "Return date must be >= departure date"
-                return False
-        if passengers > MAX_PASSENGERS:
+                result = False
+        if passengers > 8:
             print "Limit of passengers: 8"
-            return False
-    return True
+            result = False
+    return result
 
 
-validation(*argv[1:])
-scrape()
+scrape(*argv[1:])
